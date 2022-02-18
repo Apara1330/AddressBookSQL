cce9ee7c1520c314617c5045632b5f225bbf906a
Apara1330 <polapara1330@gmail.com>
2022-02-18 13:53:44 +0530

    [Apara]- AddressBook Table created

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,2 +1,11 @@
 -------------UC1------------------------------
 create database AddressBookService;
+-------------UC2-------------------------------
+create table AddressBook(First_name varchar(200),
+Last_name varchar(200), 
+Address varchar(500), 
+City varchar(200),
+State varchar(500),
+Zip bigint, 
+Phone_number varchar(500),
+Email varchar(200));
